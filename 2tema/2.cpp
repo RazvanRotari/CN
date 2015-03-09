456f46ba3a2d409d1a24aae2e29c0883f291f3ea
Rotari Razvan-Gabriel <razvanrotari@gmail.com>
2015-03-09 20:52:30 +0200

    I found X

--- 2tema/2.cpp
+++ 2tema/2.cpp
@@ -7,6 +7,14 @@
 using namespace arma;
 using namespace std;
 
+ostream& operator<<(ostream& os, const vector<double>& obj) {
+    for(auto elem:obj) {
+        os << elem << " "; 
+    }
+    os << endl;
+    return os;
+}
+
 class LU {
     private:
         mat A;
@@ -50,27 +58,128 @@ class LU {
             this->m = m;
             this->b = b;
             eps = pow(10, -m);
+            assert(check());
         }
 
-        double div1(double a) {
-            assert(abs(a) > eps);
-            return 1/a;
+        double div(double a, double b) {
+            assert(abs(b) > eps);
+            return a/b;
         }
+
+        bool check() {
+            for (size_t i = 0; i < A.n_rows; i++) {
+                mat submat = A.submat(0, 0, i, i);
+                double d = det(A);
+                if (d == 0) return false;
+            }
+            return true;
+        }
+
+        void LUdecomposition() {
+            for (int p = 0; p < A.n_rows; p++) {
+                calculateU(p);
+                calculateL(p);
+            }
+        
+        }
+
+        void calculateU(int p) {
+            for (int i = p; i < A.n_rows; i++) {
+                double u = Ainit(p, i);
+                double sum = 0;
+                for (int k = 0; k < p; k++) {
+                    sum += getL(p, k, p) * getU(k, i, p);
+                }
+                u -= sum;
+                A(p, i) = u;
+            }
+        }
+
+        void calculateL(int p) {
+            for (int i = p; i < A.n_rows; i++) {
+                double l = Ainit(i, p);
+                double sum = 0;
+                for (int k = 0; k < p; k++) {
+                    sum += getL(i, k, p) * getU(k, p, p);
+                }
+                l -= sum;
+                double temp  =  div(l, A(p, p));
+                if (i == p) continue;
+                A(i, p) = temp;
+            }
+        }
+
+        double getL(int i, int j, int p = -1) {
+            if (i == j) return 1;
+            if (j == p) return 0;
+            return A(i, j);
+        }
+
+        double getU(int i, int j, int p = -1) {
+            if (i == p) return 0;
+            return A(i, j);
+
+        }
+
+        double LUdet() {
+            double prod = 1;
+            for (int i = 0; i < A.n_rows; i++) {
+                prod *= A(i, i);
+            }
+            return prod * 1; // U * L;
+        }
+
+        vector<double> findTheX() {
+            double size = A.n_rows;
+            vector<double> ys = vector<double>(size, 0);
+            ys[0] = div(b[0], getL(0, 0));
+
+            for (int i = 1; i < size; i++) {
+                double temp = 0;
+                double sum = 0;
+                for (int j = 0; j < i; j++) {
+                    sum += getL(i, j) * ys[j];
+                }
+                temp = b[i] - sum;
+                temp = div(temp, getL(i, i));
+                ys[i] = temp;
+            }
+
+            cout << "YS " << ys;
+
+            vector<double> xs = vector<double>(size, 0);
+            double n = size - 1;
+            xs[n] = div(ys[n], getU(n ,n));
+
+            for (int i = n - 1; i >= 0; i--) {
+                double temp = 0;
+                double sum = 0;
+                for (int j = i + 1; j < size; j++) {
+                    sum += getU(i, j) * xs[j];
+                }
+                temp = ys[i] - sum;
+                temp = div(temp, getU(i, i));
+                xs[i] = temp;
+            }
+            return xs;
+        }
+
+    public:
         friend ostream& operator<<(ostream& os, const LU& obj);
 };
 
 
 ostream& operator<<(ostream& os, const LU& obj) {
     os << obj.A << endl;
-    os << obj.Ainit;
 
     return os;
 }
 
-
 int main() {
 
     LU lu = LU::from_file("data.txt");
+    lu.LUdecomposition();
     cout << lu;
+    cout << lu.findTheX();
     return 0;
 }
